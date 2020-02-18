66cb7ae9e58fc9c54f16d1f26a9e85acf6e5bd19
mb21917 <mb21917@zz20164008536F57365B.userreverse.dion.ne.jp>
2020-02-18 15:52:47 +0900

    Объявили функцию, копирующую шаблон

--- js/main.js
+++ js/main.js
@@ -46,3 +46,13 @@ var getPhotos = function (amount) {
   }
   return photos;
 };
+
+var renderPicture = function (picture) {
+  var pictureElement = pictureTemplate.cloneNode(true);
+
+  pictureElement.querySelector('.picture__img').src = picture.url;
+  pictureElement.querySelector('.picture__likes').textContent = picture.likes;
+  pictureElement.querySelector('.picture__comments').textContent = picture.comments.length;
+
+  return pictureElement;
+};
