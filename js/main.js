3e463cde1ffe9f4c531fa7bbd59931ff76a0da42
kaiere <chupeykina.olga@gmail.com>
2020-03-03 21:03:22 +0900

    Скрыли окно с большой фотографией

--- js/main.js
+++ js/main.js
@@ -72,7 +72,7 @@ var createPhoto = function (photos) {
 createPhoto(collectedPhotos);
 
 var bigPicture = document.querySelector('.big-picture');
-bigPicture.classList.remove('hidden');
+// bigPicture.classList.remove('hidden');
 
 var commentsList = document.querySelector('.social__comments');
 
