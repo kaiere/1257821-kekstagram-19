5f2e9c1d482cbc9c3e7ce4906e48228518dcbe4c
kaiere <chupeykina.olga@gmail.com>
2020-03-03 21:28:50 +0900

    Добавили обработчики - переключение фильтров по клику

--- js/main.js
+++ js/main.js
@@ -202,3 +202,41 @@ var object = {
 };
 
 effectLevelSlider.classList.add('hidden');
+
+effectsList.addEventListener('click', function (evt) {
+  if (evt.target.type === 'radio') {
+
+    if (!photoUpload.className) {
+      photoUpload.classList.add(object[evt.target.id]);
+    }
+    if (photoUpload.className !== evt.target.id) {
+      photoUpload.className = '';
+      photoUpload.classList.add(object[evt.target.id]);
+    }
+    if (photoUpload.className === object['effect-none']) {
+      effectLevelSlider.classList.add('hidden');
+    } else {
+      effectLevelSlider.classList.remove('hidden');
+    }
+
+    if (photoUpload.className === object['effect-chrome']) {
+      photoUpload.style.filter = 'grayscale(1)';
+    }
+    if (photoUpload.className === object['effect-sepia']) {
+      photoUpload.style.filter = 'sepia(1)';
+    }
+    if (photoUpload.className === object['effect-marvin']) {
+      photoUpload.style.filter = 'invert(100%)';
+    }
+    if (photoUpload.className === object['effect-phobos']) {
+      photoUpload.style.filter = 'blur(3px)';
+    }
+    if (photoUpload.className === object['effect-heat']) {
+      photoUpload.style.filter = 'brightness(3)';
+    }
+  }
+});
+
+effectsNoneBtn.addEventListener('click', function () {
+  photoUpload.style = '';
+});
