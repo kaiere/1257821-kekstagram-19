2d144e77eba6bb3f813b45b2153aa8aefce599b3
kaiere <chupeykina.olga@gmail.com>
2020-03-05 20:25:28 +0900

    Добавили в функцию отрисовки фото присовение описания к фото

--- js/main.js
+++ js/main.js
@@ -58,6 +58,7 @@ var renderPicture = function (picture) {
   pictureElement.querySelector('.picture__img').src = picture.url;
   pictureElement.querySelector('.picture__likes').textContent = picture.likes;
   pictureElement.querySelector('.picture__comments').textContent = picture.comments.length;
+  pictureElement.querySelector('.picture__img').alt = picture.description;
 
   return pictureElement;
 };
