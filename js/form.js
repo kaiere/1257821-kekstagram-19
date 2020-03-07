7f481e427f4613bcbf4e2f1b14617e59aea9b0a5
kaiere <chupeykina.olga@gmail.com>
2020-03-07 18:18:31 +0900

    Создали отдельный файл для загрузки фото и поместили туда соответствующий код

---
+++ js/form.js
@@ -0,0 +1,42 @@
+'use strict';
+
+// Загрузка фотки и редактирование
+
+(function () {
+  var photoUploadInput = document.querySelector('.img-upload .img-upload__input');
+var photoUploadCancel = document.querySelector('.img-upload .img-upload__cancel');
+var photoUploadOverlay = document.querySelector('.img-upload .img-upload__overlay');
+
+var openUploadWindow = function () {
+  photoUploadOverlay.classList.remove('hidden');
+  document.querySelector('body').classList.add('modal-open');
+};
+
+var closeUploadWindow = function () {
+  photoUploadOverlay.classList.add('hidden');
+  document.querySelector('body').classList.remove('modal-open');
+  photoUploadInput = '';
+};
+
+photoUploadInput.addEventListener('change', function () {
+  openUploadWindow();
+});
+
+photoUploadCancel.addEventListener('click', function () {
+  closeUploadWindow();
+});
+
+photoUploadCancel.addEventListener('keydown', function (evt) {
+  if (evt.key === window.utils.ENTER_KEY) {
+    closeUploadWindow();
+  }
+});
+
+var onUploadPressEscape = function (evt) {
+  if (evt.key === window.utils.ESC_KEY) {
+    closeUploadWindow();
+  }
+};
+
+document.addEventListener('keydown', onUploadPressEscape);
+})();
