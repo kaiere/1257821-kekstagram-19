8f2934f6b55ca70171dff05756500ba5a267a051
kaiere <chupeykina.olga@gmail.com>
2020-03-07 18:19:25 +0900

    Убрали лишний код

--- js/main.js
+++ js/main.js
@@ -1,42 +1,5 @@
 'use strict';
 
-var photoUploadInput = document.querySelector('.img-upload .img-upload__input');
-var photoUploadCancel = document.querySelector('.img-upload .img-upload__cancel');
-var photoUploadOverlay = document.querySelector('.img-upload .img-upload__overlay');
-
-var openUploadWindow = function () {
-  photoUploadOverlay.classList.remove('hidden');
-  document.querySelector('body').classList.add('modal-open');
-};
-
-var closeUploadWindow = function () {
-  photoUploadOverlay.classList.add('hidden');
-  document.querySelector('body').classList.remove('modal-open');
-  photoUploadInput = '';
-};
-
-photoUploadInput.addEventListener('change', function () {
-  openUploadWindow();
-});
-
-photoUploadCancel.addEventListener('click', function () {
-  closeUploadWindow();
-});
-
-photoUploadCancel.addEventListener('keydown', function (evt) {
-  if (evt.key === window.utils.ENTER_KEY) {
-    closeUploadWindow();
-  }
-});
-
-var onUploadPressEscape = function (evt) {
-  if (evt.key === window.utils.ESC_KEY) {
-    closeUploadWindow();
-  }
-};
-
-document.addEventListener('keydown', onUploadPressEscape);
-
 var smallScaleButton = document.querySelector('.scale__control--smaller');
 var bigScaleButton = document.querySelector('.scale__control--bigger');
 var scaleControlInput = document.querySelector('.scale__control--value');
