a50e04591a5cf476ff6ead27fa6b93cb29ceb882
kaiere <chupeykina.olga@gmail.com>
2020-03-10 22:57:36 +0900

    Написали функцию для перемещения пина слайдера и соответствующего изменения насыщенности эффекта

--- js/slider.js
+++ js/slider.js
@@ -7,4 +7,43 @@
   var effectLevelInput = uploadForm.querySelector('.effect-level__value');
   var effectDepth = effectLevelLine.querySelector('.effect-level__depth');
   pin.style.cursor = 'pointer';
+
+  var onPinMouseDown = function (evt) {
+    evt.preventDefault();
+
+    var onPinMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+
+      var lineStart = effectLevelLine.offsetLeft - pin.offsetWidth;
+      var lineEnd = effectLevelLine.offsetLeft + effectLevelLine.offsetWidth - pin.offsetWidth;
+      var pinPosition = pin.offsetLeft + moveEvt.movementX;
+      var limitMoveX = {
+        min: lineStart,
+        max: lineEnd
+      };
+
+      if (pinPosition < limitMoveX.min) {
+        pinPosition = limitMoveX.min;
+      }
+      if (pinPosition > limitMoveX.max) {
+        pinPosition = limitMoveX.max;
+      }
+      pin.style.left = pinPosition + 'px';
+      var effectInPercent = Math.floor((pinPosition / limitMoveX.max) * 100);
+      effectLevelInput.value = effectInPercent;
+      effectDepth.style.width = effectInPercent + '%';
+
+      var activeEffect = document.querySelector('input[name="effect"]:checked').id;
+      window.filter.setFilter(activeEffect);
+    };
+
+    var onPinMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+      document.removeEventListener('mousemove', onPinMouseMove);
+      document.removeEventListener('mouseup', onPinMouseUp);
+    };
+
+    document.addEventListener('mousemove', onPinMouseMove);
+    document.addEventListener('mouseup', onPinMouseUp);
+  };
 })();
