ea9dead49903a4b2064aea7e7d9d43cb9e9f8964
kaiere <chupeykina.olga@gmail.com>
2020-03-10 22:47:30 +0900

    Добавили новую константу - максимальный уровень насыщенности фильтра

--- js/utils.js
+++ js/utils.js
@@ -37,6 +37,7 @@
   var ESC_KEY = 'Escape';
   var ENTER_KEY = 'Enter';
   var STEP_VALUES = ['25', '50', '75', '100'];
+  var MAX_EFFECT_LEVEL = 100;
   var getRandomNumber = function (min, max) {
     var rand = min + Math.random() * (max + 1 - min);
     return Math.floor(rand);
@@ -54,6 +55,7 @@
     ESC_KEY: ESC_KEY,
     ENTER_KEY: ENTER_KEY,
     STEP_VALUES: STEP_VALUES,
+    MAX_EFFECT_LEVEL: MAX_EFFECT_LEVEL,
     getRandomNumber: getRandomNumber
   };
 })();
