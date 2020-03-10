f0b0419d3699d702f4b7a0a3c1d9cb5c93feeedd
kaiere <chupeykina.olga@gmail.com>
2020-03-10 23:08:04 +0900

    Код для формы также требует доработки - удалили старый код, добавили переменные

--- js/form.js
+++ js/form.js
@@ -3,40 +3,15 @@
 // Загрузка фотки и редактирование
 
 (function () {
-  var photoUploadInput = document.querySelector('.img-upload .img-upload__input');
-  var photoUploadCancel = document.querySelector('.img-upload .img-upload__cancel');
-  var photoUploadOverlay = document.querySelector('.img-upload .img-upload__overlay');
-
-  var openUploadWindow = function () {
-    photoUploadOverlay.classList.remove('hidden');
-    document.querySelector('body').classList.add('modal-open');
-  };
-
-  var closeUploadWindow = function () {
-    photoUploadOverlay.classList.add('hidden');
-    document.querySelector('body').classList.remove('modal-open');
-    photoUploadInput = '';
-  };
-
-  photoUploadInput.addEventListener('change', function () {
-    openUploadWindow();
-  });
-
-  photoUploadCancel.addEventListener('click', function () {
-    closeUploadWindow();
-  });
-
-  photoUploadCancel.addEventListener('keydown', function (evt) {
-    if (evt.key === window.utils.ENTER_KEY) {
-      closeUploadWindow();
-    }
-  });
-
-  var onUploadPressEscape = function (evt) {
-    if (evt.key === window.utils.ESC_KEY) {
-      closeUploadWindow();
-    }
-  };
-
-  document.addEventListener('keydown', onUploadPressEscape);
+  var body = document.querySelector('body');
+  var uploadForm = document.querySelector('.img-upload__form');
+  var photoUploadOverlay = uploadForm.querySelector('.img-upload__overlay');
+  var hashtagsInput = uploadForm.querySelector('.text__hashtags');
+  var uploadFileInput = uploadForm.querySelector('#upload-file');
+  var photoUploadCancel = uploadForm.querySelector('.img-upload__cancel');
+  var descriptionInput = uploadForm.querySelector('.text__description');
+  var effectsList = document.querySelector('.effects__list');
+  var line = uploadForm.querySelector('.effect-level__line');
+  var pin = line.querySelector('.effect-level__pin');
+  var imagePreview = photoUploadOverlay.querySelector('.img-upload__preview');
 })();
