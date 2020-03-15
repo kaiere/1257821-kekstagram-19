88ee0a753fdda1ddd723a4325fbf24f9d15197ed
kaiere <chupeykina.olga@gmail.com>
2020-03-15 20:49:41 +0900

    Исправили замечания Трэвиса

--- js/backend.js
+++ js/backend.js
@@ -1,42 +1,42 @@
 'use strict';
 
 (function () {
-var StatusCode = {
-  OK: 200
-};
-var URL = 'https://js.dump.academy/kekstagram/data';
-var TIMEOUT_IN_MS = 10000;
-var photosArray = [];
+  var StatusCode = {
+    OK: 200
+  };
+  var URL = 'https://js.dump.academy/kekstagram/data';
+  var TIMEOUT_IN_MS = 10000;
+  var photosArray = [];
 
-var loadData = function (successHandler) {
-  var xhr = new XMLHttpRequest();
-  xhr.responseType = 'json';
+  var loadData = function (successHandler) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
 
-  xhr.addEventListener('load', function () {
-    if (xhr.status === StatusCode.OK) {
-      successHandler(xhr.response);
-    } else {
-      throw new Error('Произошла ошибка: ' + xhr.status + ' ' + xhr.statusText);
-    }
-  });
+    xhr.addEventListener('load', function () {
+      if (xhr.status === StatusCode.OK) {
+        successHandler(xhr.response);
+      } else {
+        throw new Error('Произошла ошибка: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
 
-  xhr.addEventListener('error', function () {
-    throw new Error('Произошла ошибка соединения');
-  });
+    xhr.addEventListener('error', function () {
+      throw new Error('Произошла ошибка соединения');
+    });
 
-  xhr.addEventListener('timeout', function () {
-    throw new Error('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-  });
+    xhr.addEventListener('timeout', function () {
+      throw new Error('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
 
-  xhr.timeout = TIMEOUT_IN_MS;
+    xhr.timeout = TIMEOUT_IN_MS;
 
-  xhr.open('GET', URL);
-  xhr.send();
-};
+    xhr.open('GET', URL);
+    xhr.send();
+  };
 
-window.load = {
-  loadData: loadData,
-  photosArray: photosArray,
-  StatusCode: StatusCode
-};
+  window.load = {
+    loadData: loadData,
+    photosArray: photosArray,
+    StatusCode: StatusCode
+  };
 })();
