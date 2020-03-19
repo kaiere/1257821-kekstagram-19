e706420b2544f8ca3c509e7d2b0730051481a250
kaiere <chupeykina.olga@gmail.com>
2020-03-19 16:48:46 +0900

    Отредактировали код для отрисовки фотографий после фильтрации

--- js/gallery.js
+++ js/gallery.js
@@ -8,27 +8,31 @@
 
   var renderPicture = function (picture, id) {
     var pictureElement = pictureTemplate.cloneNode(true);
-
     pictureElement.querySelector('.picture__img').src = picture.url;
     pictureElement.querySelector('.picture__likes').textContent = picture.likes;
     pictureElement.querySelector('.picture__comments').textContent = picture.comments.length;
     pictureElement.dataset.id = id;
     return pictureElement;
   };
-
   var createPhotosArray = function (array) {
     var fragment = document.createDocumentFragment();
     for (var j = 0; j < array.length; j++) {
       fragment.appendChild(renderPicture(array[j], j));
     }
-
     picturesContainer.appendChild(fragment);
   };
-
   var successLoadHandler = function (data) {
     window.load.photosArray = data;
     createPhotosArray(window.load.photosArray);
+    window.filtration.getRandomPhotos();
+    window.filtration.getDefaultPhotos();
+    window.filtration.getDiscussedPhotos();
   };
 
   window.load.loadData(successLoadHandler);
-})();
+
+  window.gallery = {
+    picturesContainer: picturesContainer,
+    createPhotosArray: createPhotosArray
+  };
+  })();
